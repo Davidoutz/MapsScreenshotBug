b1043e25f5e21c9079fa43bf1d45081abf3b9d1c
David B <david.beauverd@gmail.com>
2023-01-24 10:00:15 +0100

    Ajoutez des fichiers projet.

---
+++ MapsScreenshotBug/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MapsScreenshotBug;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        IScreenshotResult screen = await map.CaptureAsync();
+        Stream stream = await screen.OpenReadAsync();
+        var image = ImageSource.FromStream(() => stream);
+        screenshot.Source = image;
+    }
+}
+
---
+++ MapsScreenshotBug/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MapsScreenshotBug;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
